561cb3927c0464400d5023b003f2f8a1469adb27
skdding3 <skdding3@gmail.com>
2022-12-15 23:21:29 +0900

    Feat: Next blog datafetching setting

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,14 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
+export async function getStaticProps() {
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
+
 export default function Home() {
   return (
       <Layout home>
@@ -9,7 +17,7 @@ export default function Home() {
           <title>{siteTitle}</title>
         </Head>
         <section className={utilStyles.headingMd}>
-          <p>[Your Self Introduction]</p>
+          <p>[I'am Front-End Developer]</p>
           <p>
             (This is a sample website - you’ll be building a site like this on{' '}
             <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
