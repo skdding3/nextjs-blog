a2058a21f0385eea25aa306799918cd5b4cf7ef4
skdding3 <skdding3@gmail.com>
2022-12-16 16:11:30 +0900

    Feat: pre-rendering

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,38 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+// import {getSortedPostsData} from "../lib/posts";
+import {useState,useEffect} from "react";
 
-export async function getStaticProps() {
-    return {
-        props: {
-            allPostsData
-        }
-    }
-}
+/**
+ * SSG pre-rendering
+ * @return {Promise<{props: {allPostsData}}>}
+ */
+// export async function getStaticProps() {
+//     const allPostsData = getSortedPostsData()
+//     return {
+//         props: {
+//             allPostsData
+//         }
+//     }
+// }
 
 export default function Home() {
+
+    /**
+     * CSR pre-rendering
+     */
+    // State
+    const [allPostsData, setAllPostsData] = useState([])
+
+    // Effect
+    useEffect(() => {
+        fetch("/api/posts")
+            .then((res) => res.json())
+            .then((data) => setAllPostsData(data.allPostsData))
+    },[])
+
+
   return (
       <Layout home>
         <Head>
@@ -23,6 +45,20 @@ export default function Home() {
             <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
           </p>
         </section>
+          <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+              <h2 className={utilStyles.headingLg}>Blog</h2>
+              <ul className={utilStyles.list}>
+                  {allPostsData.map(({ id, date, title }) => (
+                      <li className={utilStyles.listItem} key={id}>
+                          {title}
+                          <br />
+                          {id}
+                          <br />
+                          {date}
+                      </li>
+                  ))}
+              </ul>
+          </section>
       </Layout>
   );
 }
