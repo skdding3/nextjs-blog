0902a27824e18dce1e60a02e0359cc384f8a4bd5
skdding3 <skdding3@gmail.com>
2022-12-17 17:18:39 +0900

    Dynamic Routes Setting

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,16 @@
 import Head from "next/head";
 import Layout from "../../components/Layout";
+import { getAllPostIds } from '../../lib/posts';
 
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false,
+    };
+}
 
-export default function FirstPost() {
+export default function Post() {
     return (
         <Layout>
         <Head>
