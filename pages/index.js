5238e2fefacdb05bdef2ab249d9d476ea4c6c814
skdding3 <skdding3@gmail.com>
2022-12-19 01:23:06 +0900

    polishing

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 // import {getSortedPostsData} from "../lib/posts";
 import {useState,useEffect} from "react";
+import Link from 'next/link';
+import Date from '../components/Date';
 
 /**
  * SSG pre-rendering
@@ -50,11 +52,11 @@ export default function Home() {
               <ul className={utilStyles.list}>
                   {allPostsData.map(({ id, date, title }) => (
                       <li className={utilStyles.listItem} key={id}>
-                          {title}
+                          <Link href={`/posts/${id}`}>{title}</Link>
                           <br />
-                          {id}
-                          <br />
-                          {date}
+                          <small className={utilStyles.lightText}>
+                              <Date dateString={date} />
+                          </small>
                       </li>
                   ))}
               </ul>
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Layout from "../../components/Layout";
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from "../../components/Date";
+import utilStyles from '../../styles/utils.module.css';
 
 
 // 경로에는 getAllPostIds()에서 반환한 알려진 경로 배열이 포함됩니다.
@@ -25,13 +27,17 @@ export async function getStaticProps({ params }) {
 export default function Post({postData}) {
     return (
         <Layout>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            {postData.data}
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+            </div>
             <br />
             <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     )
 }
