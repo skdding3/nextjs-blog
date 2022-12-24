35a902c57b06e83821e99fbfe428eef05f24027e
skdding3 <skdding3@gmail.com>
2022-12-24 21:25:15 +0900

    tailwind setting

--- components/Layout.js
+++ components/Layout.js
@@ -9,6 +9,7 @@ export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
     return (
+        <div className="bg-pink-100">
         <div className={styles.container}>
             <Head>
                 <link rel="icon" href="/favicon.ico" />
@@ -64,6 +65,7 @@ export default function Layout({ children, home }) {
                     <Link href="/">← Back to home</Link>
                 </div>
             )}
+        </div>
         </div>
     );
 }
